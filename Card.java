4e2f138899a572a7e18086290213ee9e0e02afe6
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-03-05 22:10:43 +0530

    added code

--- Card.java
+++ Card.java
@@ -1,1 +1,35 @@
 //Card.java
+public class Card {
+    private String rank;
+    private String suit;
+
+    // Constructor
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    // Getters
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    // String representation of a card
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
+    // Check if two cards have the same suit
+    public boolean sameSuit(Card other) {
+        return this.suit.equals(other.suit);
+    }
+
+    // Check if two cards have the same rank
+    public boolean sameRank(Card other) {
+        return this.rank.equals(other.rank);
+    }
+}
