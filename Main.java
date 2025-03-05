26d492c742100790e0033ca2c6034d8589db3063
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-03-05 22:12:04 +0530

    added code

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
-//Main.java
+// Main.java
+// Tests the Card and Deck classes
+
+public class Main {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.shuffle();
+
+        Card card1 = deck.drawCard();
+        Card card2 = deck.drawCard();
+
+        System.out.println("Drawn cards:");
+        System.out.println(card1);
+        System.out.println(card2);
+
+        System.out.println("Same Suit? " + card1.sameSuit(card2));
+        System.out.println("Same Rank? " + card1.sameRank(card2));
+    }
+}
